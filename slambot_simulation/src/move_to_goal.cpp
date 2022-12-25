657def0f1017516b02f1d99e56d6c5548881b55a
Shobuj Paul <shobuj.paul@gmail.com>
2022-12-25 16:18:34 +0530

    added complete c++ code for navigating to goal using slam

--- slambot_simulation/src/move_to_goal.cpp
+++ slambot_simulation/src/move_to_goal.cpp
@@ -4,35 +4,36 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "simple_navigation_goals");
 
   
-  MoveBaseClient ac("move_base", true);
+    MoveBaseClient ac("move_base", true);
 
   
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
 
-  move_base_msgs::MoveBaseGoal goal;
+    move_base_msgs::MoveBaseGoal goal;
 
   
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
 
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+    goal.target_pose.pose.position.x = 1.0;
+    goal.target_pose.pose.orientation.w = 1.0;
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
 
-  ac.waitForResult();
+    ac.waitForResult();
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Action Success");
-  else
-    ROS_INFO("Action Failed");
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Action Success");
+    else
+        ROS_INFO("Action Failed");
 
-  return 0;
+    return 0;
 }
